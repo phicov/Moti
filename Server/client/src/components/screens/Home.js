25315225fc99bbebd443b25b104cd111c815d451
Philippe Covington <philippehcov@gmail.com>
2022-07-29 09:20:51 -0400

    Home UI

--- Server/client/src/components/screens/Home.js
+++ Server/client/src/components/screens/Home.js
@@ -2,7 +2,69 @@ import React from "react";
 
 const Home = () =>{
   return(
-    <h1>home</h1>
+    <div className="home">
+      <div className="card home-card">
+        <h5></h5>
+        <div className="card-image">
+          <img src="https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__480.jpg"/>
+        </div>
+        <div className="card-content">
+        <i className="material-icons" style={{color:"red"}}>favorite</i>
+
+          <h6>title</h6>
+          <p>caption</p>
+          <input type="text" placeholder="add a comment"/>
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5></h5>
+        <div className="card-image">
+          <img src="https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__480.jpg"/>
+        </div>
+        <div className="card-content">
+        <i className="material-icons" style={{color:"red"}}>favorite</i>
+          <h6>title</h6>
+          <p>caption</p>
+          <input type="text" placeholder="add a comment"/>
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5></h5>
+        <div className="card-image">
+          <img src="https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__480.jpg"/>
+        </div>
+        <div className="card-content">
+          <i className="material-icons" style={{color:"red"}}>favorite</i>
+          <h6>title</h6>
+          <p>caption</p>
+          <input type="text" placeholder="add a comment"/>
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5></h5>
+        <div className="card-image">
+          <img src="https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__480.jpg"/>
+        </div>
+        <div className="card-content">
+        <i className="material-icons" style={{color:"red"}}>favorite</i>
+          <h6>title</h6>
+          <p>caption</p>
+          <input type="text" placeholder="add a comment"/>
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5></h5>
+        <div className="card-image">
+          <img src="https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__480.jpg"/>
+        </div>
+        <div className="card-content">
+        <i className="material-icons" style={{color:"red"}}>favorite</i>
+          <h6>title</h6>
+          <p>caption</p>
+          <input type="text" placeholder="add a comment"/>
+        </div>
+      </div>
+    </div>
   );
 }
 
