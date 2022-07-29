765cda91fee2309d00579ad58303caafe75ad17a
Philippe Covington <philippehcov@gmail.com>
2022-07-29 08:49:08 -0400

    Profile UI Finished

--- Server/client/src/components/screens/Profile.js
+++ Server/client/src/components/screens/Profile.js
@@ -2,7 +2,41 @@ import React from "react";
 
 const Profile = () =>{
   return(
-  <h1>Profile</h1>
+  <div style={{maxWidth:"100%",margin:"0px auto"}}>
+    <div style = {{
+      display:"flex",
+      justifyContent:"space-around",
+      margin:"20px 20px",
+      borderBottom:"1px solid grey"
+    }}>
+      <div>
+          <img style={{width:"30%", borderRadius:"100%"}}
+          src="https://repository-images.githubusercontent.com/444436968/03ab929b-0265-4e8c-97c8-e8578f02030a"
+          />
+      </div>
+      <div>
+        <h4>Zoro</h4>
+        <div style={{
+          display:"flex",
+          justifyContent:"space-between",
+          width:"108%"
+      }}>
+          <h5>40 posts</h5>
+          <h5>40 followers</h5>
+          <h5>40 following</h5>
+        </div>
+      </div>
+    </div>
+    <div className="gallery">
+      <img className="item" src="https://repository-images.githubusercontent.com/444436968/03ab929b-0265-4e8c-97c8-e8578f02030a"/>
+      <img className="item" src="https://repository-images.githubusercontent.com/444436968/03ab929b-0265-4e8c-97c8-e8578f02030a"/>
+      <img className="item" src="https://repository-images.githubusercontent.com/444436968/03ab929b-0265-4e8c-97c8-e8578f02030a"/>
+      <img className="item" src="https://repository-images.githubusercontent.com/444436968/03ab929b-0265-4e8c-97c8-e8578f02030a"/>
+      <img className="item" src="https://repository-images.githubusercontent.com/444436968/03ab929b-0265-4e8c-97c8-e8578f02030a"/>
+      <img className="item" src="https://repository-images.githubusercontent.com/444436968/03ab929b-0265-4e8c-97c8-e8578f02030a"/>
+      <img className="item" src="https://repository-images.githubusercontent.com/444436968/03ab929b-0265-4e8c-97c8-e8578f02030a"/>
+    </div>
+  </div>
   
   );
 }
